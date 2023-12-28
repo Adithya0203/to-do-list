797f772a8934f5e4f6523cc1093d0bf8df704c3d
S Adithya <adithyasrikumar03@gmail.com>
2023-12-28 14:11:28 +0530

    finished checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,8 @@ import { useState } from 'react'
 import "./App.css"
 import Paper from '@mui/material/Paper'
 import Grid from '@mui/material/Grid'
-<<<<<<< HEAD
 import InputArea from './assets/components/InputArea';
 import ToDoItem from './assets/components/ToDoItem'
-=======
-import Checkbox from '@mui/material/Checkbox';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
-import Typography from '@mui/material/Typography';
-import InputArea from './assets/components/InputArea';
-import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
-import { Container, Paper } from '@mui/material';
->>>>>>> 569e2a63f9b1add1b32630abe663300fa63a3ef0
 
 function App() {
   const[items,setItems] = useState([])
@@ -34,7 +23,6 @@ function App() {
   }
 
   return (
-<<<<<<< HEAD
     <div>
       <div className="bg1"></div>
       <div className="bg2"></div>
@@ -55,17 +43,6 @@ function App() {
         </Paper>
       </Grid>
     </div>
-=======
-    <Container>
-      <div className="bg1"></div>
-      <div className="bg2"></div>
-      <Grid display="flex" justifyContent="center" alignItems="center" height="100dvh">
-        <Card raised="true">
-          <InputArea />
-        </Card>
-      </Grid>
-    </Container>
->>>>>>> 569e2a63f9b1add1b32630abe663300fa63a3ef0
   )
 }
 
